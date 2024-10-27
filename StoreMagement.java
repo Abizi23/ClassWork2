1e1d02cea38089030c7c5050dd7f5961ec355461
Abizi <abielght87@gmail.com>
2024-10-27 13:50:08 +0300

    created a teest class

---
+++ StoreMagement.java
@@ -0,0 +1,10 @@
+import java.util.Arraylist;
+import java.util.List;
+
+public class StoreMagement {
+    public static void main(String[] args) {
+        List<StoreItem>storeItems=new Arraylist<>();
+        storeItems.add(new Reagent("HCl",50,1,0.9,"08/12/2025"));
+    }
+    
+}

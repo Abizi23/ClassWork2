6f543694fb0ee4a4f9db0d8d97f6db26fa50f0fa
Abizi <abielght87@gmail.com>
2024-10-27 12:14:11 +0300

    created a class of reagent

---
+++ Reagent.java
@@ -0,0 +1,19 @@
+
+    public class Reagent extends StoreItem{
+        private double purity;
+        private String expiry;
+    
+        
+        
+        
+        
+    
+        
+    
+        
+        
+        
+       
+       
+    }
+    

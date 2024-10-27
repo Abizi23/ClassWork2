0cfc6ca777aeb6e7754be93e375513af4d9e55b0
Abizi <abielght87@gmail.com>
2024-10-27 17:50:17 +0300

    removed the instance variable quantity

--- Equipment.java
+++ Equipment.java
@@ -1,10 +1,20 @@
 public class Equipment extends StoreItem{
     private int serialNumber;
 
-    public Equipment(String name, double price, int quantity, int serialNumber) {
-        super(name, price, quantity);
+    public Equipment(String name, double price, int serialNumber) {
+        super(name, price);
         this.serialNumber = serialNumber;
     }
 
     
+
+    public void show(){
+        System.out.println("Equipment Specification:"+" "+getName()+" "+getSerialNumber()+": "+getPrice()+"ugx"+": "+" ");
+    }
+
+
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
 }

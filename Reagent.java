e98945862e0d6e51a6fe74ba611fb9d33feaf539
Abizi <abielght87@gmail.com>
2024-10-27 12:21:18 +0300

    added constructor

--- Reagent.java
+++ Reagent.java
@@ -2,7 +2,14 @@
     public class Reagent extends StoreItem{
         private double purity;
         private String expiry;
+
+        public Reagent(String name, double price, int quantity, double purity, String expiry) {
+            super(name, price, quantity);
+            this.purity = purity;
+            this.expiry = expiry;
+        }
     
+        
         
         
         

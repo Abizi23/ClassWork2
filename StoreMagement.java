b89bb512174984f1b1adb824a2910a189642702c
Abizi <abielght87@gmail.com>
2024-10-27 17:36:54 +0300

    added new kit object

--- StoreMagement.java
+++ StoreMagement.java
@@ -5,6 +5,8 @@ public class StoreMagement {
     public static void main(String[] args) {
         List<StoreItem>storeItems=new Arraylist<>();
         storeItems.add(new Reagent("HCl",50,1,0.9,"08/12/2025"));
+        storeItems.add(new Kit("hcv",5,100,5));
+        
     }
     
 }

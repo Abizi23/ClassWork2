4010f932f1af4f83c557498b1fab47ffb2cbc076
Abizi <abielght87@gmail.com>
2024-10-27 17:58:15 +0300

    Fixed bug
    small letter was used in Arraylist and made it to ArrayList

--- StoreMagement.java
+++ StoreMagement.java
@@ -1,11 +1,12 @@
-import java.util.Arraylist;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StoreMagement {
     public static void main(String[] args) {
-        List<StoreItem>storeItems=new Arraylist<>();
+        List<StoreItem>storeItems=new ArrayList<>();
         storeItems.add(new Reagent("HCl",50,1,0.9,"08/12/2025"));
-        storeItems.add(new Kit("hcv",5,100,5));
+        storeItems.add(new Kit("hcv",5,100,5,));
+        storeItems.add(new Equipment("Chemsitry Analyzer",5000,241026));
         
     }
     

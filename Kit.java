dd41e9579faab3609cbaa93b70ea62e9e4b8428f
Abizi <abielght87@gmail.com>
2024-10-27 13:33:30 +0300

    implemented the abstract method

--- Kit.java
+++ Kit.java
@@ -1,6 +1,24 @@
 public class Kit extends StoreItem{
 
     private int numberofbox;
+
+    public Kit(String name, double price, int quantity, int numberofbox) {
+        super(name, price, quantity);
+        this.numberofbox = numberofbox;
+    }
+    
+
+    
+        public void show(){
+            System.out.println("Kit Specification:"+" "+getName()+" "+getNumberofbox()+": "+getPrice()+"ugx"+": "+" "+getQuantity()+"ml")
+        }
+
+
+
+        public int getNumberofbox() {
+            return numberofbox;
+        }
+    }
     
     
 }
